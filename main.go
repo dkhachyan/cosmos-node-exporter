3f7ea3e40f15f2e0940e542f3a5691eec55db1f8
Denis Khachyan <khachyanda@gmail.com>
2022-06-10 19:10:12 +0300

    host from cmd param

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
@@ -9,17 +10,17 @@ import (
 )
 
 func main() {
+	host := flag.String("host", "tcp://127.0.0.1:26658", "host to connect")
+	flag.Parse()
 
-	host := "tcp://127.0.0.1:26658"
-
-	col, err := NewNodeCollector(host)
+	col, err := NewNodeCollector(*host)
 	if err != nil {
 		log.Fatal(err)
 	}
 	prometheus.MustRegister(col)
 
 	http.Handle("/metrics", promhttp.Handler())
-	log.Printf("Using host %v", host)
+	log.Printf("Using host %v", *host)
 	log.Printf("Beginning to serve on port :9001")
 	log.Fatal(http.ListenAndServe(":9001", nil))
 }
