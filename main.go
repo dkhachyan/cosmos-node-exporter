75a6f17982f1342be0ce30f013cb18734eef7169
d.khachyan <d.khachyan@kryptonite.ru>
2022-06-10 14:47:44 +0300

    do not panic if host is unreachable

--- main.go
+++ main.go
@@ -9,12 +9,17 @@ import (
 )
 
 func main() {
+
 	host := "tcp://127.0.0.1:26658"
 
-	col := NewNodeCollector(host)
+	col, err := NewNodeCollector(host)
+	if err != nil {
+		log.Fatal(err)
+	}
 	prometheus.MustRegister(col)
 
 	http.Handle("/metrics", promhttp.Handler())
+	log.Printf("Using host %v", host)
 	log.Printf("Beginning to serve on port :9001")
 	log.Fatal(http.ListenAndServe(":9001", nil))
 }
--- metrics.go
+++ metrics.go
@@ -12,10 +12,15 @@ import (
 type NodeCollector struct {
 	latest_block_height *prometheus.Desc
 	latest_block_time   *prometheus.Desc
-	host                string
+	rpcClient           *rpchttp.HTTP
+	ctx                 context.Context
 }
 
-func NewNodeCollector(node string) *NodeCollector {
+func NewNodeCollector(host string) (*NodeCollector, error) {
+	rpcClient, err := rpchttp.New(host)
+	if err != nil {
+		return nil, err
+	}
 	return &NodeCollector{
 		latest_block_height: prometheus.NewDesc("latest_block_height",
 			"Shows latest block height",
@@ -25,8 +30,9 @@ func NewNodeCollector(node string) *NodeCollector {
 			"Shows latest block time",
 			nil, nil,
 		),
-		host: node,
-	}
+		rpcClient: rpcClient,
+		ctx:       context.TODO(),
+	}, nil
 }
 
 func (collector *NodeCollector) Describe(ch chan<- *prometheus.Desc) {
@@ -37,23 +43,19 @@ func (collector *NodeCollector) Describe(ch chan<- *prometheus.Desc) {
 
 func (collector *NodeCollector) Collect(ch chan<- prometheus.Metric) {
 
-	status, err := getStatus(collector.host)
+	status, err := collector.getStatus()
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("Host %v is unreachable", collector.rpcClient.Remote())
+		return
 	}
-
 	ch <- prometheus.MustNewConstMetric(collector.latest_block_height, prometheus.CounterValue, float64(status.SyncInfo.LatestBlockHeight))
 	ch <- prometheus.MustNewConstMetric(collector.latest_block_time, prometheus.CounterValue, float64(status.SyncInfo.LatestBlockTime.Unix()))
 
 }
 
-func getStatus(host string) (*coretypes.ResultStatus, error) {
-	rpcClient, err := rpchttp.New(host)
-	if err != nil {
-		return nil, err
-	}
+func (collector *NodeCollector) getStatus() (*coretypes.ResultStatus, error) {
 
-	status, err := rpcClient.Status(context.TODO())
+	status, err := collector.rpcClient.Status(collector.ctx)
 	if err != nil {
 		return nil, err
 	}
